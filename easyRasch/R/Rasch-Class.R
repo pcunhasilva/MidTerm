c6c3419de17f1ab5d4c150187830c9205546feb9
pcunhasilva <patricksilva27@gmail.com>
2017-03-26 15:08:25 -0500

    Generate Class Rasch
    Generate the Class Rasch;
    Create the initialize method;
    Create a setValidity
    Create a Generic for Rasch

---
+++ easyRasch/R/Rasch-Class.R
@@ -0,0 +1,53 @@
+#' Generate class Rasch
+#' 
+#' An object of the class Rasch must contain:
+#' \itemize{
+#' \item \code{name} The name of the test taker
+#' \item \code{a} A vector of the question-item paramenters a (difficulty of the question) 
+#' \item \code{y} A vector of answers for the respondent \eqn{y_j}
+#' }
+#' 
+#' @author Patrick C. Silva: \email{pcunhasilva@wustl.edu}
+#' @aliases Squares-Rasch initialize,Rasch-method 
+#' @rdname Rasch
+#' @export
+setClass(Class = "Rasch",
+         representation = representation(
+            name = "character",
+            a = "numeric",
+            y = "numeric"),
+         prototype = prototype(
+            name = character(),
+            a = numeric(),
+            y = numeric()
+         )
+)
+
+#' @export  
+setMethod("initialize", "Rasch",
+          function(.Object, ...){
+             value = callNextMethod()
+             return(value)
+          }
+)
+
+#' @export
+setValidity("Rasch", function(object){
+   # Test if a and y have the same lenght
+   testlength <- length(object@y)==length(object@a)
+   # Return an error message if one of the tests returns FALSE.
+   if (!testlength){return(" a and y do not have the same length")}
+}
+)
+
+#' @export
+setGeneric("Rasch",
+           function(x){
+              standardGeneric("Rasch")
+           }
+)
+
+
+
+
+
